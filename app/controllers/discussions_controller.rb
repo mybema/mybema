f96a9e284da9e8d8657b0c06c7550da9f2579ddb
Pawel Janiak <brand.magnate@gmail.com>
2014-07-24 09:36:51 +0200

    Add show action to discussions. Also fetch categories.

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,4 +1,6 @@
 class DiscussionsController < ApplicationController
+  before_action :fetch_categories, only: [:show, :index, :edit, :new]
+
   def index
     @discussions = Discussion.visible
 
@@ -6,5 +8,15 @@ class DiscussionsController < ApplicationController
       @category    = DiscussionCategory.find(params[:discussion_category_id])
       @discussions = @category.discussions
     end
+  end
+
+  def show
+    @discussion = Discussion.find(params[:id])
+  end
+
+  private
+
+  def fetch_categories
+    @categories = DiscussionCategory.all
   end
 end
--- test/controllers/discussions_controller_test.rb
+++ test/controllers/discussions_controller_test.rb
@@ -7,6 +7,13 @@ class DiscussionsControllerTest < ActionController::TestCase
     assert_response :success, @response.body
   end
 
+  test "GET index assigns all categories" do
+    category_one = create(:discussion_category)
+    category_two = create(:discussion_category)
+    get :index
+    assert_equal [category_one, category_two], assigns(:categories)
+  end
+
   test "GET index assigns all visible discussions" do
     first_discussion  = create(:discussion)
     second_discussion = create(:discussion)
@@ -34,5 +41,19 @@ class DiscussionsControllerTest < ActionController::TestCase
     second_discussion = create(:discussion)
     get :index, discussion_category_id: category.id
     assert_equal [first_discussion], assigns(:discussions)
+  end
+
+  test "GET show assigns the discussion" do
+    discussion = create(:discussion)
+    get :show, id: discussion.id, discussion_category_id: discussion.discussion_category.id
+    assert_equal discussion, assigns(:discussion)
+  end
+
+  test "GET show assigns all categories" do
+    category_one = create(:discussion_category)
+    category_two = create(:discussion_category)
+    discussion   = create(:discussion, discussion_category: category_one)
+    get :show, discussion_category_id: category_one.id, id: discussion.id
+    assert_equal [category_two, category_one], assigns(:categories)
   end
 end
