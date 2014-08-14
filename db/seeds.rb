7575c5763b8f381f5055f122c17e79d325561615
Pawel Janiak <brand.magnate@gmail.com>
2014-08-14 20:01:39 +0200

    Remove puts statement from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
 # Create guest user
 unless User.where(username: 'Guest').any?
-  puts 'hi'
   User.create name: 'Guest', username: 'Guest', email: 'guest@mybema.com', password: SecureRandom.hex(12)
 end
