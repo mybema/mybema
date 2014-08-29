9ba6b7b07c210efd703cc6910bb68990c2c56064
Pawel Janiak <brand.magnate@gmail.com>
2014-08-29 19:49:05 +0200

    Limit the homepage to 8 discussions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @discussions = Discussion.with_includes.visible.by_recency
+    @discussions = Discussion.with_includes.visible.by_recency.limit(8)
   end
 end
