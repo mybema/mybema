e5c8ced922f08a80f7bb9728abcc164662257000
Pawel Janiak <brand.magnate@gmail.com>
2014-09-27 23:21:02 +0200

    Force index import and refresh for search test.

--- test/features/search/search_test.rb
+++ test/features/search/search_test.rb
@@ -6,8 +6,7 @@ class Search < Capybara::Rails::TestCase
     category   = create(:discussion_category, name: 'Cool category')
     discussion = create(:discussion, discussion_category: category,
                                      user: guest, title: 'Good chat', body: 'This is very informative')
-    Discussion.import
-    Discussion.__elasticsearch__.refresh_index!
+    Discussion.import force: true, refresh: true
     visit root_path
     fill_in 'query', with: 'chat'
     click_button 'SEARCH'
@@ -18,8 +17,7 @@ class Search < Capybara::Rails::TestCase
     guest = create(:user, username: 'Guest')
     create(:discussion, user: guest, title: 'Good chat', body: 'Will show')
     create(:discussion, user: guest, hidden: true, title: 'Good chat', body: 'Will not show')
-    Discussion.import
-    Discussion.__elasticsearch__.refresh_index!
+    Discussion.import force: true, refresh: true
     visit root_path
     fill_in 'query', with: 'chat'
     click_button 'SEARCH'
