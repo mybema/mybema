4bfb46628c1843288936d4147e221f11ed82e1ce
Pawel Janiak <brand.magnate@gmail.com>
2014-09-27 15:39:55 +0200

    Annotate guidelines factory

--- test/factories/guidelines.rb
+++ test/factories/guidelines.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: guidelines
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 FactoryGirl.define do
   factory :guideline do
     name Faker::Lorem.sentence(4)
