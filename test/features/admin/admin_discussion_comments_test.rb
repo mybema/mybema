1fe6ebedebaeab53d1f681e46db60e0b60284e79
Pawel Janiak <brand.magnate@gmail.com>
2014-09-29 17:55:56 +0200

    Add comment actions and status to admin list, add specs, add edit page.

--- test/features/admin/admin_discussion_comments_test.rb
+++ test/features/admin/admin_discussion_comments_test.rb
@@ -19,4 +19,46 @@ class AdminDiscussionCommentsTest < Capybara::Rails::TestCase
     click_link 'Very insightful'
     assert_content page, 'A random discussion'
   end
+
+  test 'admin can update an existing comment' do
+    create(:discussion_comment, body: 'Some weird comment')
+    visit admin_comments_path
+    click_link 'manage'
+    fill_in 'discussion_comment_body', with: 'No longer weird'
+    click_button 'update comment'
+    assert_content page, 'No longer weird'
+    assert_content page, 'manage'
+  end
+
+  test 'admin can hide a comment' do
+    create(:discussion_comment)
+    visit admin_comments_path
+    click_link 'manage'
+    click_link 'hide comment'
+    assert_content page, 'This comment is hidden'
+  end
+
+  test 'admin can unhide a comment' do
+    create(:discussion_comment, hidden: true)
+    visit admin_comments_path
+    click_link 'manage'
+    click_link 'show comment'
+    refute_content page, 'This comment is hidden'
+  end
+
+  test 'admin sees the comment visibility in the index listing' do
+    create(:discussion_comment, hidden: true)
+    create(:discussion_comment)
+    visit admin_comments_path
+    assert_content page, 'Hidden'
+    assert_content page, 'Visible'
+  end
+
+    test 'admin can delete an existing comment' do
+    create(:discussion_comment, body: 'Will be removed')
+    visit admin_comments_path
+    click_link 'manage'
+    click_link 'delete comment'
+    assert_equal DiscussionComment.count, 0
+  end
 end
