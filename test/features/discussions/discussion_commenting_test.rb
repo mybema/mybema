b571e30565478ec5fbbeea0a997fdfa3a0938f29
Pawel Janiak <brand.magnate@gmail.com>
2014-09-29 17:41:05 +0200

    Admins can see link to comment manage page for hidden comments.

--- test/features/discussions/discussion_commenting_test.rb
+++ test/features/discussions/discussion_commenting_test.rb
@@ -7,8 +7,7 @@ class DiscussionCommenting < Capybara::Rails::TestCase
     discussion = create(:discussion, discussion_category: category,
                                      user: guest, title: 'Good chat', body: 'This is very informative')
     create(:discussion_comment, body: 'In before 2 cents', discussion: discussion)
-    visit root_path
-    click_link 'Good chat'
+    visit discussion_path(discussion)
     assert_content page, 'In before 2 cents'
     fill_in 'Have something to add?', with: 'My very valuable 2 cents'
     click_button 'Respond'
@@ -21,10 +20,36 @@ class DiscussionCommenting < Capybara::Rails::TestCase
     discussion = create(:discussion, discussion_category: category,
                                      user: guest, title: 'Good chat', body: 'This is very informative')
     create(:discussion_comment, body: 'A hidden comment', discussion: discussion, hidden: true)
-    visit root_path
-    click_link 'Good chat'
+    visit discussion_path(discussion)
     refute_content page, 'A hidden comment'
     assert_content page, 'This response has been removed'
+  end
+
+  test 'user cannot see the link to the comment management page for a hidden comment' do
+    guest      = create(:user, username: 'Guest')
+    category   = create(:discussion_category, name: 'Cool category')
+    discussion = create(:discussion, discussion_category: category,
+                                     user: guest, title: 'Good chat', body: 'This is very informative')
+    create(:discussion_comment, body: 'A hidden comment', discussion: discussion, hidden: true)
+    visit discussion_path(discussion)
+    refute_content page, 'View comment in dashboard'
+  end
+
+  test 'admin can see the link to the comment management page for a hidden comment' do
+    guest      = create(:user, username: 'Guest')
+    admin      = create(:admin, name: 'Super Admin', email: 'admin@test.com', password: 'password')
+    category   = create(:discussion_category, name: 'Cool category')
+    discussion = create(:discussion, discussion_category: category,
+                                     user: guest, title: 'Good chat', body: 'This is very informative')
+    create(:discussion_comment, body: 'A hidden comment', discussion: discussion, hidden: true)
+    visit root_path
+    click_link 'Log in'
+    click_link 'Admin sign in'
+    fill_in 'Email', with: 'admin@test.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Sign in'
+    visit discussion_path(discussion)
+    assert_content page, 'View comment in dashboard'
   end
 
   test 'admin can respond to discussion comment on actual discussion page' do
