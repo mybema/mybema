22aea7437f3f34d9583ee4bcc84b6ac99eb121e4
Pawel Janiak <brand.magnate@gmail.com>
2014-10-06 20:09:10 +0200

    Override devise controller to transfer content

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,13 +4,50 @@ class RegistrationsController < Devise::RegistrationsController
     super
   end
 
+  def create
+    build_resource(sign_up_params)
+
+    resource_saved = resource.save
+    yield resource if block_given?
+    if resource_saved
+      transfer_guest_content resource
+      remove_guest_cookie
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      @validatable = devise_mapping.validatable?
+      if @validatable
+        @minimum_password_length = resource_class.password_length.min
+      end
+      respond_with resource
+    end
+  end
+
   private
+
+  def account_update_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password)
+  end
+
+  def remove_guest_cookie
+    cookies.delete :mybema_guest_id
+  end
 
   def sign_up_params
     params.require(:user).permit(:username, :email, :ip_address, :password, :password_confirmation)
   end
 
-  def account_update_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password)
+  def transfer_guest_content resource
+    if guest_id = cookies[:mybema_guest_id]
+      ContentTransferWorker.perform_async resource.id, guest_id
+    end
   end
 end
