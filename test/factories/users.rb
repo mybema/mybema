711abb7efa753f838f2aca5cbc0bea7e045d7227
Pawel Janiak <brand.magnate@gmail.com>
2014-10-24 19:54:46 +0200

    Fix user factory

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -25,10 +25,10 @@
 
 FactoryGirl.define do
   factory :user do
-    name Faker::Name.name
+    name { Faker::Name.name }
     sequence(:email) { |n| Faker::Internet.email + "#{n}" }
     password Faker::Internet.password
-    username Faker::Internet.user_name
+    username { Faker::Internet.user_name }
     ip_address Faker::Internet.ip_v4_address
   end
 end
