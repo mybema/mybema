1091f6b756adc94f9ce5fb5efd0fa86304c425a2
Pawel Janiak <brand.magnate@gmail.com>
2014-10-25 12:24:41 +0200

    Add admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,13 @@ unless User.where(username: 'Guest').any?
   User.create name: 'Guest', username: 'Guest', email: 'guest@mybema.com', password: SecureRandom.hex(12)
 end
 
+# Clear all guidelines and add the defaults
 Guideline.delete_all
 ["Add a short, relevant title to your discussion.", "Be clear and concise.",
 "Check your spelling and grammar before posting.", "Add your discussion to the appropriate category.",
 "Be civil. We don't tolerate hate speech, racism or personal insults.",
 "Do not lie or stretch the truth.", "No posting of spam. Obviously.",
 "Thoughtful comments are much better than 'me too' posts."].each { |name| Guideline.create(name: name) }
+
+# Create admin account
+Admin.create(name: 'Admin', email: 'admin@admin.admin', password: 'password')
