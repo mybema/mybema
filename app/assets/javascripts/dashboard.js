ff30db960d3c5f6faf7d4605fa1843b0556faf1e
Pawel Janiak <brand.magnate@gmail.com>
2014-11-03 19:58:25 +0200

    Allow admins to dismiss flash messages.

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -7,9 +7,15 @@
 //= require typeahead_implementation.js
 //= require_self
 
-// Make entire row clickable as opposed to just the link text
 jQuery(document).ready(function($) {
+
+  // Make entire row clickable as opposed to just the link text
   $(".js-clickable-row").click(function() {
     window.document.location = $(this).data("url");
+  });
+
+  // dismiss flash messages
+  $('#dismiss-flash-msg').click(function() {
+    $('#flash-notices').hide();
   });
 });
