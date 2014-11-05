ba9a93a4131163e0920e8c3469f4ef5ae8b4c317
Pawel Janiak <brand.magnate@gmail.com>
2014-11-05 20:06:30 +0200

    Add flash messages to comments controller

--- app/controllers/discussion_comments_controller.rb
+++ app/controllers/discussion_comments_controller.rb
@@ -3,9 +3,11 @@ class DiscussionCommentsController < ApplicationController
     @comment = DiscussionComment.new comment_params
 
     if @comment.save
+      flash[:notice] = 'Your response was added'
       create_identicon('DiscussionComment', @comment.id)
       redirect_to discussion_path(@comment.discussion)
     else
+      flash[:alert] = 'Your response could not be added'
       redirect_to discussion_path(@comment.discussion)
     end
   end
