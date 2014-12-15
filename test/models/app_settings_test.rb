1ffb819aa4613f5ea28ea7aad71cc6c5b5b023d6
Pawel Janiak <brand.magnate@gmail.com>
2014-12-16 00:33:10 +0200

    Add AppSettings annotations

--- test/models/app_settings_test.rb
+++ test/models/app_settings_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: app_settings
+#
+#  id                  :integer          not null, primary key
+#  all_articles_img    :string(255)
+#  all_discussions_img :string(255)
+#  join_community_img  :string(255)
+#  new_discussion_img  :string(255)
+#  logo                :string(255)
+#  hero_message        :text
+#  created_at          :datetime
+#  updated_at          :datetime
+#
+
 require 'test_helper'
 
 class AppSettingsTest < ActiveSupport::TestCase
