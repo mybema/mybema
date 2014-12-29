35e2d616b94dde4890dab2af016baf085bc45b74
Pawel Janiak <brand.magnate@gmail.com>
2014-12-29 09:24:57 +0200

    Add IE8 CSS compatibility fixes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(
+  ie8-and-down.css
   dashboard.css
   dashboard.js
 )
