45407aaf548b6947667a22d9c8a86ac6af2f4b91
Pawel Janiak <brand.magnate@gmail.com>
2014-12-30 15:07:48 +0200

    Make discussion titles unique.
    This is to avoid the problem of having name clashes because then slugs
    won’t be unique and all discussions with the same name will route to the
    first discussion.

--- test/factories/discussions.rb
+++ test/factories/discussions.rb
@@ -20,7 +20,7 @@ FactoryGirl.define do
   factory :discussion do
     user
     discussion_category
-    body Faker::Lorem.sentence(20)
-    title Faker::Lorem.sentence(4)
+    body { Faker::Lorem.sentence(20) }
+    title { Faker::Lorem.sentence(4) }
   end
 end
