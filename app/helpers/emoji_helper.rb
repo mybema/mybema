1e62197cda14e1654f787c73e7ecce159dcebaf4
Pawel Janiak <brand.magnate@gmail.com>
2014-12-30 18:01:52 +0200

    Convert common smilies to emojis i.e :) => :smile:

--- app/helpers/emoji_helper.rb
+++ app/helpers/emoji_helper.rb
@@ -1,11 +1,19 @@
 module EmojiHelper
   def emojify(content)
-    h(content).to_str.gsub(/:([\w+-]+):/) do |match|
+    h(alias_content(content)).to_str.gsub(/:([\w+-]+):/) do |match|
       if emoji = Emoji.find_by_alias($1)
         %(<img alt="#$1" src="#{asset_path("emoji/#{emoji.image_filename}")}" style="vertical-align:middle" width="20" height="20" />)
       else
         match
       end
     end.html_safe if content.present?
+  end
+
+  def alias_content content
+    content.gsub!(":)", ':smile:')
+    content.gsub!(";)", ':wink:')
+    content.gsub!(":(", ':confused:')
+    content.gsub!(":'(", ':cry:')
+    content
   end
 end
