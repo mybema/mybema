8dcd9ad320f240ba5bf9ac7121bac402b15917db
Pawel Janiak <brand.magnate@gmail.com>
2015-01-02 23:18:47 +0200

    Move the email configuration back to the custom mailer.
    Rails 4 supports passing in a deliver_method_options key as part of its
    options hash. Moving the config back here also means that emails that
    are queued in Sidekiq will also work without a hitch.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :app_settings, :fetch_user_and_handle_guest_cookie
-  before_filter :configure_action_mailer_settings
 
   def app_settings
     @app ||= AppSettings.first
@@ -25,19 +24,6 @@ class ApplicationController < ActionController::Base
   end
 
   private
-
-  def configure_action_mailer_settings
-    ActionMailer::Base.smtp_settings = {
-      :address              => app_settings.smtp_address,
-      :port                 => app_settings.smtp_port,
-      :domain               => app_settings.smtp_domain,
-      :user_name            => app_settings.smtp_username,
-      :password             => app_settings.smtp_password,
-      :authentication       => "plain",
-      :enable_starttls_auto => true
-    }
-    ActionMailer::Base.default_url_options[:host] = app_settings.domain_address
-  end
 
   def create_identicon(klass, object_id)
     IdenticonWorker.perform_async(klass, object_id) if @current_user.guest?
--- app/mailers/mybema_devise_mailer.rb
+++ app/mailers/mybema_devise_mailer.rb
@@ -1,22 +1,18 @@
 class MybemaDeviseMailer < Devise::Mailer
+  before_filter :set_default_host
+
   def invitation_instructions(record, token, opts={})
-    opts[:from] = AppSettings.first.mailer_sender
-    opts[:reply_to] = AppSettings.first.mailer_reply_to
-    @token = token
+    set_email_opts(opts, token)
     devise_mail(record, :invitation_instructions, opts)
   end
 
   def confirmation_instructions(record, token, opts={})
-    @token = token
-    opts[:from] = AppSettings.first.mailer_sender
-    opts[:reply_to] = AppSettings.first.mailer_reply_to
+    set_email_opts(opts, token)
     devise_mail(record, :confirmation_instructions, opts)
   end
 
   def reset_password_instructions(record, token, opts={})
-    @token = token
-    opts[:from] = AppSettings.first.mailer_sender
-    opts[:reply_to] = AppSettings.first.mailer_reply_to
+    set_email_opts(opts, token)
     devise_mail(record, :reset_password_instructions, opts)
   end
 
@@ -25,13 +21,42 @@ class MybemaDeviseMailer < Devise::Mailer
     mail(to: record.email,
          from: AppSettings.first.mailer_sender,
          template_path: "devise/mailer",
-         subject: "Welcome to the community!")
+         subject: "Welcome to the community!",
+         delivery_method_options: app_smtp_settings)
   end
 
   def unlock_instructions(record, token, opts={})
-    @token = token
+    set_email_opts(opts, token)
+    devise_mail(record, :unlock_instructions, opts)
+  end
+
+  private
+
+  def set_email_opts opts, token
     opts[:from] = AppSettings.first.mailer_sender
     opts[:reply_to] = AppSettings.first.mailer_reply_to
-    devise_mail(record, :unlock_instructions, opts)
+    opts[:delivery_method_options] = app_smtp_settings
+
+    @token = token
+  end
+
+  def set_default_host
+    default_url_options[:host] = smtp_settings.domain_address
+  end
+
+  def smtp_settings
+    @app_settings || AppSettings.first
+  end
+
+  def app_smtp_settings
+    self.smtp_settings = {
+      address:              smtp_settings.smtp_address,
+      port:                 smtp_settings.smtp_port,
+      domain:               smtp_settings.smtp_domain,
+      user_name:            smtp_settings.smtp_username,
+      password:             smtp_settings.smtp_password,
+      authentication:       "plain",
+      enable_starttls_auto: true
+    }
   end
 end
