ab668cc03c4dedb8e26f2d6a194149c7f7de4283
Pawel Janiak <brand.magnate@gmail.com>
2015-01-10 16:25:49 +0200

    Improve discussion comments controller test names

--- test/controllers/discussion_comments_controller_test.rb
+++ test/controllers/discussion_comments_controller_test.rb
@@ -6,20 +6,20 @@ class DiscussionCommentsControllerTest < ActionController::TestCase
     create(:app_settings)
   end
 
-  test "POST create will create a new comment" do
+  test "POST create will create a new comment as a guest if guest posting is enabled" do
     discussion = create(:discussion, id: 4)
     post :create, discussion_comment: { body: 'The comment body', user_id: 5, discussion_id: 4 }
     assert_equal DiscussionComment.last.body, 'The comment body'
   end
 
-  test "POST create will not create a new comment if guest posting is turned off" do
+  test "POST create will not create a new comment as guest if guest posting is disabled" do
     AppSettings.first.update_attributes(guest_posting: false)
     discussion = create(:discussion, id: 4)
     post :create, discussion_comment: { body: 'The comment body', user_id: 5, discussion_id: 4 }
     assert_equal DiscussionComment.count, 0
   end
 
-  test "POST create redirects to the discussion" do
+  test "POST create redirects to the discussion when created successfully" do
     discussion = create(:discussion, id: 4)
     post :create, discussion_comment: { body: 'The body', title: 'slug', user_id: 5, discussion_id: 4 }
     assert_redirected_to discussion_path(discussion.slug)
